72ed9ac769895128411ca7cf3f4cfa673a8b2337
Slobodan Dan <danastroman@gmail.com>
2023-06-21 15:35:25 +0200

    Typescript rewrite.

--- README.md
+++ README.md
@@ -1,13 +1,4 @@
-Munkres implementation for Javascript
----------------------------------
-
-![Bower Version](https://img.shields.io/bower/v/munkres-js.svg?style=flat)
-[![NPM Version](https://img.shields.io/npm/v/munkres-js.svg?style=flat)](https://npmjs.org/package/munkres-js)
-[![NPM Downloads](https://img.shields.io/npm/dm/munkres-js.svg?style=flat)](https://npmjs.org/package/munkres-js)
-[![Build Status](https://travis-ci.org/addaleax/munkres-js.svg?style=flat&branch=master)](https://travis-ci.org/addaleax/munkres-js?branch=master)
-[![Coverage Status](https://coveralls.io/repos/addaleax/munkres-js/badge.svg?branch=master)](https://coveralls.io/r/addaleax/munkres-js?branch=master)
-[![Dependency Status](https://david-dm.org/addaleax/munkres-js.svg?style=flat)](https://david-dm.org/addaleax/munkres-js)
-[![devDependency Status](https://david-dm.org/addaleax/munkres-js/dev-status.svg?style=flat)](https://david-dm.org/addaleax/munkres-js#info=devDependencies)
+## Munkres implementation for Javascript
 
 ## Introduction
 
@@ -21,36 +12,37 @@ used more than once.
 
 [Hungarian algorithm]: https://en.wikipedia.org/wiki/Hungarian_algorithm
 
+[Youtube video](https://www.youtube.com/watch?v=cQ5MsiGaDY8)
+
 ## Usage
 
-```js
-var munkres = require('munkres-js');
+```ts
+import { computeMunkres } from 'munkrest-ts';
 
-munkres([
-  [400, 150, 400],
-  [400, 450, 600],
-  [300, 225, 300]
-])
+computeMunkres([
+    [400, 150, 400],
+    [400, 450, 600],
+    [300, 225, 300],
+]);
 // => [ [ 0, 1 ], [ 1, 0 ], [ 2, 2 ] ]
 ```
 
 Returns the list of matrix indices corresponding to the optimal assignment.
 
-When used in the browser, the global `computeMunkres` function is exposed.
-
-See the docs in munkres.js for more details.
-
 ## Meta
 
-This module is a translation of a Python implementation by
+This module is a Typescript version of the module `munkres-js` by
+[Anna Henningsen] (https://github.com/addaleax/munkres-js)
+
+which is in turn a translation of a Python implementation by
 [Brian Clapper](https://github.com/bmc/munkres).
 
 The original implementation is based on
 <http://csclab.murraystate.edu/~bob.pilgrim/445/munkres.html>.
 
-It is available via `bower` and `npm` as `munkres-js`.
-
 ## Copyright
+
+&copy; 2023 Slobodan Dan (Conversion to Typescript)
 
 &copy; 2014 Anna Henningsen (Conversion to JS)
 
---
+++ __test__/munkres.test.ts
@@ -0,0 +1,125 @@
+/**
+ * A Mocha unit-test for munkress-js
+ *
+ * @author Erel Segal-Halevi
+ * @since 2016-03
+ */
+
+import { computeMunkres, profitToCostMatrix } from '../munkres';
+
+describe('Munkres Algorithm', function () {
+    it('handles singleton matrix', function () {
+        const matrix = [[5]];
+        expect(computeMunkres(matrix)).toEqual([[0, 0]]);
+    });
+
+    it('handles negative singleton matrix', function () {
+        const matrix = [[-5]];
+        expect(computeMunkres(matrix)).toEqual([[0, 0]]);
+    });
+
+    it('handles 2-by-2 matrix', function () {
+        const matrix = [
+            [5, 3],
+            [2, 4],
+        ];
+        expect(computeMunkres(matrix)).toEqual([
+            [0, 1],
+            [1, 0],
+        ]); // smallest cost is 3+2=5
+    });
+
+    it('handles 2-by-2 negative matrix', function () {
+        const matrix = [
+            [-5, -3],
+            [-2, -4],
+        ];
+        expect(computeMunkres(matrix)).toEqual([
+            [0, 0],
+            [1, 1],
+        ]); // smallest cost is -5-4=-9
+    });
+
+    it('handles 3-by-3 matrix', function () {
+        const matrix = [
+            [5, 3, 1],
+            [2, 4, 6],
+            [9, 9, 9],
+        ];
+
+        expect(computeMunkres(matrix)).toEqual([
+            [0, 2],
+            [1, 0],
+            [2, 1],
+        ]); // smallest cost is 1+2+9=12
+    });
+
+    it('handles another 3-by-3 matrix', function () {
+        const matrix = [
+            [400, 150, 400],
+            [400, 450, 600],
+            [300, 225, 300],
+        ];
+
+        expect(computeMunkres(matrix)).toEqual([
+            [0, 1],
+            [1, 0],
+            [2, 2],
+        ]); // smallest cost is 150+400+300=850
+    });
+
+    it('handles 3-by-3 matrix with both positive and negative values', function () {
+        const matrix = [
+            [5, 3, -1],
+            [2, 4, -6],
+            [9, 9, -9],
+        ];
+
+        expect(computeMunkres(matrix)).toEqual([
+            [0, 1],
+            [1, 0],
+            [2, 2],
+        ]); // smallest cost is -1+2+9=10
+    });
+
+    it('handles all-zero 3-by-3 matrix', function () {
+        const matrix = [
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0],
+        ];
+
+        expect(computeMunkres(matrix)).toEqual([
+            [0, 0],
+            [1, 1],
+            [2, 2],
+        ]); // smallest cost is 0
+    });
+
+    it('handles rectangular 3-by-4 matrix', function () {
+        const matrix = [
+            [400, 150, 400, 1],
+            [400, 450, 600, 2],
+            [300, 225, 300, 3],
+        ];
+
+        expect(computeMunkres(matrix)).toEqual([
+            [0, 1],
+            [1, 3],
+            [2, 0],
+        ]); // smallest cost is 150+2+300=452
+    });
+
+    it('converts profit-matrix to cost-matrix', function () {
+        const profitmatrix = [
+            [5, 3],
+            [2, 4],
+        ];
+        const costmatrix = profitToCostMatrix(profitmatrix);
+
+        expect(computeMunkres(costmatrix)).toEqual([
+            [0, 0],
+            [1, 1],
+        ]); // largest profit is 5+4=9
+    });
+});
